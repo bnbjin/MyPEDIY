c93b49061ed99a7b5cce86864d94a6ad4abbcb53
paradoximov <hejinyuan1993@gmail.com>
2017-03-16 17:41:05 +0800

    shell.cpp: 完成初步实现。

--- MyPEDIY/shell.cpp
+++ MyPEDIY/shell.cpp
@@ -10,8 +10,6 @@
 */
 int ImployShell(void* _pImageBase, void** _ppShellSection)
 {
-	const PIMAGE_NT_HEADERS pNTHeader = getNTHeader(_pImageBase);
-	const PIMAGE_SECTION_HEADER pLastSecHeader = getLastSecHeader(_pImageBase);
 	unsigned long shellrawsize = (unsigned long)(&Label_Shell_End) - (unsigned long)(&Label_Shell_Start);
 
 	CreateNewSection(_pImageBase, shellrawsize, _ppShellSection);
@@ -19,14 +17,27 @@ int ImployShell(void* _pImageBase, void** _ppShellSection)
 	// 把shell的数据写入shell映像中
 	memcpy(*_ppShellSection, (&Label_Shell_Start), shellrawsize);
 
-
+	const PIMAGE_NT_HEADERS pNTHeader = getNTHeader(_pImageBase);
+	const PIMAGE_SECTION_HEADER pLastSecHeader = getLastSecHeader(_pImageBase);
+	
+	
 	/*  TODO : 修复SHELL的自建输入表  */
+	/* Import Descriptor: FirstThunk, OriginalFirstThunk, Name */
+	/* Thunks */
+	PInduction_Import pInductionImp = (PInduction_Import)((unsigned long)(*_ppShellSection) + (unsigned long)(&Label_Induction_Import_Start) - (unsigned long)(&Label_Shell_Start));
+	pInductionImp->ImpD[0].FirstThunk += pLastSecHeader->VirtualAddress;
+	pInductionImp->ImpD[0].OriginalFirstThunk += pLastSecHeader->VirtualAddress;
+	pInductionImp->ImpD[0].Name += pLastSecHeader->VirtualAddress;
+	pInductionImp->Thunk[0].u1.AddressOfData += pLastSecHeader->VirtualAddress;
+	pInductionImp->Thunk[1].u1.AddressOfData += pLastSecHeader->VirtualAddress;
+	pInductionImp->Thunk[2].u1.AddressOfData += pLastSecHeader->VirtualAddress;
+	
 
 	/*  TODO : 填写shell相关数据字段  */
-	PInduction_Data pInductionData = (PInduction_Data)(&Label_Induction_Data_Start);
+	PInduction_Data pInductionData = (PInduction_Data)((unsigned long)(*_ppShellSection) + (unsigned long)(&Label_Induction_Data_Start) - (unsigned long)(&Label_Shell_Start));
 	pInductionData->LuanchBase = pLastSecHeader->VirtualAddress + (DWORD)(&Label_Luanch_Start) - (DWORD)(&Label_Shell_Start);
-	pInductionData->nLuanchPackSize = (DWORD)(&Label_Luanch_End) - (DWORD)(&Label_Shell_Start);
-	PLuanch_Data pLuanchData = (PLuanch_Data)(&Lable_Luanch_Data_Start);
+	pInductionData->nLuanchPackSize = (DWORD)(&Label_Luanch_End) - (DWORD)(&Label_Luanch_Start);
+	PLuanch_Data pLuanchData = (PLuanch_Data)((unsigned long)(*_ppShellSection) + (unsigned long)(&Lable_Luanch_Data_Start) - (unsigned long)(&Label_Shell_Start));
 	pLuanchData->OEP = pNTHeader->OptionalHeader.AddressOfEntryPoint;
 	pLuanchData->IsMutateImpTable = ISMUTATEIMPORT ? 1 : 0;
 	pLuanchData->OriginalImpTableAddr = 0;
@@ -38,10 +49,10 @@ int ImployShell(void* _pImageBase, void** _ppShellSection)
 	/*  AddressOfEntryPoint, BaseOfCode  , DataDirectory[IMPORT,IAT]*/
 	pNTHeader->OptionalHeader.AddressOfEntryPoint = pLastSecHeader->VirtualAddress;
 	pNTHeader->OptionalHeader.BaseOfCode = pLastSecHeader->VirtualAddress;
-	pNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress = pLastSecHeader->VirtualAddress + (DWORD)(&Label_Mutate_Import_Start) - (DWORD)(&Label_Shell_Start);
-	pNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].Size = (DWORD)(&Label_Mutate_Import_End) - (DWORD)(&Label_Mutate_Import_Start);
-	pNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IAT].VirtualAddress = 0;
-	pNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IAT].Size = 0;
+	pNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress = pLastSecHeader->VirtualAddress + (DWORD)(&Label_Induction_Import_Start) - (DWORD)(&Label_Shell_Start);
+	pNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].Size = (DWORD)(&Label_Induction_Import_End) - (DWORD)(&Label_Induction_Import_Start);
+	pNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IAT].VirtualAddress = pLastSecHeader->VirtualAddress + (DWORD)(&Label_Induction_Import_End) - (DWORD)(&Label_Shell_Start);
+	pNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IAT].Size = 4 * sizeof(DWORD);
 
 	return	ERR_SUCCESS;
 }
